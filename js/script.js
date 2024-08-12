d38b25a94e1266289458089b9848386219409e07
beasb <bia4sb4@gmail.com>
2024-08-12 02:45:55 -0300

    jogo com cenario

--- js/script.js
+++ js/script.js
@@ -15,14 +15,26 @@ setTimeout(() => {
 
 const loop = setInterval(() => {
 
+    console.log('loop')
+
 const batomPosition = batom.offsetLeft
 const personagemPosition = +window.getComputedStyle(personagem).bottom.replace('px', '')
 
 console.log(personagemPosition)
 
 if (batomPosition <= 100 && batomPosition > 0 && personagemPosition < 70) {
+   
     batom.style.animation = 'none'
     batom.style.left = `${batomPosition}px`
+
+    personagem.style.animation = 'none'
+    personagem.style.bottom = `${personagemPosition}px`
+
+    personagem.src = './img/gameover.png'
+    personagem.style.width = '200px'
+    personagem.style.marginLeft = '450px'
+
+    clearInterval(loop)
 }
  
 }, 10)   
